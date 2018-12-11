114c46973565df3a467add7b2ca5b57510106d58
n22550946 <n22550946@hotmail.com>
2018-12-12 02:05:56 +0800

    增加autoMapper

--- NorthWindPracticeWeb/Areas/Customer/Controllers/CustomerController.cs
+++ NorthWindPracticeWeb/Areas/Customer/Controllers/CustomerController.cs
@@ -24,6 +24,7 @@ namespace NorthWindPracticeWeb.Areas.Customer.Controllers
 
         public ActionResult List(int pageNumber = 1, int pageSize = 20)
         {
+            
             return View("_List");
         }
         public ActionResult Create()
