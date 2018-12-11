4606d5854819b348e8a25ce0cebcc660bf6cf21b
n22550946 <n22550946@hotmail.com>
2018-12-12 02:52:30 +0800

    增加參考專案Common 並建立IsNullOrEmpty測試方法

--- NorthWindPracticeWeb.Common.Test/StringExtensionTest.cs
+++ NorthWindPracticeWeb.Common.Test/StringExtensionTest.cs
@@ -1,7 +1,26 @@
-﻿namespace NorthWindPracticeWeb.Common.Test
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NorthWindPracticeWeb.Common.Extensions;
+
+namespace NorthWindPracticeWeb.Common.Test
 {
+    [TestClass]
     public class StringExtensionTest
     {
-        
+        [TestMethod]
+        [Owner("Nick")]
+        [TestCategory("StringExtension")]
+        [TestProperty("StringExtension", "IsNullOrEmpty")]
+        public void IsNullOrEmpty_輸入空字串_應為True()
+        {
+            //arrange (準備)
+            var value = "";
+
+            //act (執行)
+            var actual = value.IsNullOrEmpty();
+
+            //assert (驗證)
+            actual.Should().BeTrue();
+        }
     }
 }
