46543203be05b31cacf78b1660026abce4a64085
n22550946 <n22550946@hotmail.com>
2018-12-12 01:29:14 +0800

    增加CustomerController Index List

---
+++ NorthWindPracticeWeb/Areas/Customer/Controllers/CustomerController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NorthWindPracticeWeb.Service.Interface;
+
+namespace NorthWindPracticeWeb.Areas.Customer.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private readonly ICustomerService _customerService;
+
+        public CustomerController(ICustomerService customerService)
+        {
+            _customerService = customerService;
+        }
+
+        // GET: Customer/Customer
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult List(int pageNumber = 1, int pageSize = 20)
+        {
+            return View("_List");
+        }
+
+    }
+}
