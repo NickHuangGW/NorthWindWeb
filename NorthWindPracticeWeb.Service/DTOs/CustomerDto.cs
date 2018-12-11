fb4aec3ddf54f0604cd64dc6fc46575e07d13da2
n22550946 <n22550946@hotmail.com>
2018-12-12 00:02:52 +0800

    建立serviceDto

---
+++ NorthWindPracticeWeb.Service/DTOs/CustomerDto.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthWindPracticeWeb.Service.DTOs
+{
+    /// <summary>
+    /// CustomerDto
+    /// </summary>
+    public class CustomerDto
+    {
+        /// <summary>
+        /// Gets or sets the customer identifier.
+        /// </summary>
+        /// <value>
+        /// The customer identifier.
+        /// </value>
+        public string CustomerID { get; set; }
+        /// <summary>
+        /// Gets or sets the name of the company.
+        /// </summary>
+        /// <value>
+        /// The name of the company.
+        /// </value>
+        public string CompanyName { get; set; }
+        /// <summary>
+        /// Gets or sets the name of the contact.
+        /// </summary>
+        /// <value>
+        /// The name of the contact.
+        /// </value>
+        public string ContactName { get; set; }
+        /// <summary>
+        /// Gets or sets the contact title.
+        /// </summary>
+        /// <value>
+        /// The contact title.
+        /// </value>
+        public string ContactTitle { get; set; }
+        /// <summary>
+        /// Gets or sets the address.
+        /// </summary>
+        /// <value>
+        /// The address.
+        /// </value>
+        public string Address { get; set; }
+        /// <summary>
+        /// Gets or sets the city.
+        /// </summary>
+        /// <value>
+        /// The city.
+        /// </value>
+        public string City { get; set; }
+        /// <summary>
+        /// Gets or sets the region.
+        /// </summary>
+        /// <value>
+        /// The region.
+        /// </value>
+        public string Region { get; set; }
+        /// <summary>
+        /// Gets or sets the postal code.
+        /// </summary>
+        /// <value>
+        /// The postal code.
+        /// </value>
+        public string PostalCode { get; set; }
+        /// <summary>
+        /// Gets or sets the country.
+        /// </summary>
+        /// <value>
+        /// The country.
+        /// </value>
+        public string Country { get; set; }
+        /// <summary>
+        /// Gets or sets the phone.
+        /// </summary>
+        /// <value>
+        /// The phone.
+        /// </value>
+        public string Phone { get; set; }
+        /// <summary>
+        /// Gets or sets the fax.
+        /// </summary>
+        /// <value>
+        /// The fax.
+        /// </value>
+        public string Fax { get; set; }
+    }
+}
