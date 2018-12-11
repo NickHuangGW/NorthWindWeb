2014ba32a3a4b303e6ff6e9d1aadbe6ac9c61eb0
n22550946 <n22550946@hotmail.com>
2018-12-12 00:48:03 +0800

    增加CustomerService

---
+++ NorthWindPracticeWeb.Service/CustomerService.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using NorthWindPracticeWeb.Repository.Interfaces;
+using NorthWindPracticeWeb.Repository.Models;
+using NorthWindPracticeWeb.Service.DTOs;
+using NorthWindPracticeWeb.Service.Interface;
+
+namespace NorthWindPracticeWeb.Service
+{
+   public class CustomerService : ICustomerService
+    {
+        private readonly IGenericRepository<Customers> _customerRepository;
+
+        public CustomerService(IGenericRepository<Customers> customerRepository)
+        {
+            _customerRepository = customerRepository;
+        }
+
+        public IEnumerable<CustomerDto> GetCustomers()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void CreateCustomer(CustomerDto dtoModel)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void EditCustomer(string customerId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public CustomerDto GetCustomerById()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void DeleteCustomer(string customerId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<CustomerDto> GetCustomersWithPages(int pageNumber, int pageSize)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ NorthWindPracticeWeb.Service/Interface/ICustomerService.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using NorthWindPracticeWeb.Service.DTOs;
+
+namespace NorthWindPracticeWeb.Service.Interface
+{
+    public interface ICustomerService
+    {
+        IEnumerable<CustomerDto> GetCustomers();
+
+        void CreateCustomer(CustomerDto dtoModel);
+
+        void EditCustomer(string customerId);
+
+        CustomerDto GetCustomerById();
+
+        void DeleteCustomer(string customerId);
+
+        IEnumerable<CustomerDto> GetCustomersWithPages(int pageNumber, int pageSize);
+    }
+}
