c90051222609462804efed6ea78169c4bc59705c
n22550946 <n22550946@hotmail.com>
2018-12-11 23:38:48 +0800

    建立UnitOfWork

---
+++ NorthWindPracticeWeb.Repository/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,22 @@
+﻿using System.Data.Entity;
+
+namespace NorthWindPracticeWeb.Repository.Interfaces
+{
+    /// <summary>
+    /// IUnitOfWork
+    /// </summary>
+    public interface IUnitOfWork
+    {
+        /// <summary>
+        /// Gets or sets the unit database context.
+        /// </summary>
+        /// <value>
+        /// The unit database context.
+        /// </value>
+        DbContext UnitDbContext { get; set; }
+        /// <summary>
+        /// Commits the trans.
+        /// </summary>
+        void CommitTrans();
+    }
+}
---
+++ NorthWindPracticeWeb.Repository/UnitOfWork.cs
@@ -0,0 +1,35 @@
+﻿using System.Data.Entity;
+using NorthWindPracticeWeb.Repository.Interfaces;
+using NorthWindPracticeWeb.Repository.Models;
+
+namespace NorthWindPracticeWeb.Repository
+{
+    /// <summary>
+    /// UnitOfWork
+    /// </summary>
+    /// <seealso cref="NorthWindPracticeWeb.Repository.Interfaces.IUnitOfWork" />
+    public class UnitOfWork: IUnitOfWork
+    {
+        /// <summary>
+        /// Gets or sets the unit database context.
+        /// </summary>
+        /// <value>
+        /// The unit database context.
+        /// </value>
+        public DbContext UnitDbContext { get; set; }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UnitOfWork"/> class.
+        /// </summary>
+        public UnitOfWork()
+        {
+            UnitDbContext = new NorthWindModels();
+        }
+        /// <summary>
+        /// Commits the trans.
+        /// </summary>
+        public void CommitTrans()
+        {
+            UnitDbContext.SaveChanges();
+        }
+    }
+}
