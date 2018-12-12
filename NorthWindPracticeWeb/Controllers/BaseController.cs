600e27a4dba8e3e15d8a333dc41778751ac651cd
n22550946 <n22550946@hotmail.com>
2018-12-12 23:06:54 +0800

    增加ExceptionAttribute

--- NorthWindPracticeWeb/Controllers/BaseController.cs
+++ NorthWindPracticeWeb/Controllers/BaseController.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using NorthWindPracticeWeb.FilterAttribute;
 
 namespace NorthWindPracticeWeb.Controllers
 {
+    [ExceptionLog]
     public class BaseController : Controller
     {
 
---
+++ NorthWindPracticeWeb/FilterAttribute/ExceptionLogAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Mvc;
+
+namespace NorthWindPracticeWeb.FilterAttribute
+{
+    public sealed class ExceptionLogAttribute : System.Web.Mvc.FilterAttribute, IExceptionFilter
+    {
+        public string ErrorMessage { get; set; } = "錯誤。處理您的要求時發生錯誤。";
+
+        public void OnException(ExceptionContext filterContext)
+        {
+            if (filterContext.ExceptionHandled)
+            {
+                return;
+            }
+
+            var errorMessage = filterContext.Exception.Message;
+            //todo Log將錯誤訊息接住
+            filterContext.ExceptionHandled = true;
+            filterContext.Controller.ViewData["ErrorMessage"] = ErrorMessage;
+            filterContext.Result=new ViewResult {ViewName = "Error.cshtml" };
+        }
+    }
+}
